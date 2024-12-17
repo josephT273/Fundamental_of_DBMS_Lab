36fca6c045f6b8126707f11b04c5ae52a3ef8ba4
Josepht273 <josephtadessem@gmail.com>
2024-12-17 22:28:31 +0300

    Lab exam sql file

--- lab_exam.sql
+++ lab_exam.sql
@@ -1,21 +1,22 @@
-CREATE DATABASE LabExam;
+CREATE DATABASE University;
 
-USE LabExam;
+USE University;
 
--- Create table students
-CREATE TABLE students (
-	fullname varchar(30) not null,
-	id int IDENTITY(1, 1) primary key not null,
-	age int,
-	gender varchar(10),
-	courses int FOREIGN KEY REFERENCES Courses(courseId),
-);
 
 -- Question Four
 CREATE TABLE Courses (
 	courseId int primary key not null,
 	courseName varchar(30) not null,
 )
+
+-- Create table students
+CREATE TABLE students (
+	fullname varchar(30) not null,
+	id int primary key not null,
+	age int,
+	gender varchar(10),
+	courses int FOREIGN KEY REFERENCES Courses(courseId),
+);
 
 -- Question 5
 INSERT INTO Courses
@@ -60,3 +61,37 @@ SELECT min(age), max(age) FROM students;
 SELECT fullname, id, age, gender, courseName 
 FROM students
 INNER JOIN Courses ON students.courses = Courses.courseId
+
+CREATE PROCEDURE GetAllStudent
+AS
+BEGIN
+	SELECT * FROM students
+END
+
+EXEC GetAllStudent
+
+CREATE PROCEDURE ReturnInfo @stud_id int
+AS
+BEGIN
+	SELECT id, fullname, gender FROM students
+	WHERE id=@stud_id
+END
+
+EXEC ReturnInfo @stud_id=2
+
+
+CREATE PROCEDURE UpdateById @stud_id int, @stud_age int
+AS
+BEGIN
+	UPDATE students SET age=@stud_age WHERE id=@stud_id
+END
+
+EXEC UpdateById @stud_id=2, @stud_age=21
+
+CREATE PROCEDURE DeleteById @stud_id int
+AS
+BEGIN
+	DELETE FROM students WHERE id=@stud_id
+END
+
+EXEC DeleteById @stud_id=2
